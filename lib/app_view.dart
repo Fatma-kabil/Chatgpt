83b4e1c4ad26a037ac2d1be6a0494d6787a6a632
Fatma_kabil <fatmakhaled112@gmail.com>
2025-07-29 09:59:18 +0200

    first commit

---
+++ lib/app_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatgptApp extends StatelessWidget {
+  const ChatgptApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      
+    )
+    ;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:chat_gpt_app/app_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ChatgptApp());
+}
