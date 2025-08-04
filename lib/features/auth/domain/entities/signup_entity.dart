889db87acff3011332eb1182fefdfe702f21d8c1
Fatma_kabil <fatmakhaled112@gmail.com>
2025-08-04 09:20:52 +0200

    create login & signup entity

---
+++ lib/features/auth/domain/entities/signup_entity.dart
@@ -0,0 +1,8 @@
+class SignupEntity {
+  final String name;
+  final String email;
+  final String password;
+
+  SignupEntity({required this.name, required this.email, required this.password});
+  
+}
